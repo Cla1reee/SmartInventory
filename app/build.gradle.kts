7de9a83bcf078aa5cbdeb5995acea484ad021eab
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2026-01-16 01:04:12 +0700

    Add Material Components dependency to `build.gradle.kts`
    - Add `com.google.android.material:material:1.9.0` to the project dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,4 +93,6 @@ dependencies {
 
     // Tambahkan ini untuk RecyclerView:
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    implementation("com.google.android.material:material:1.9.0")
 }
